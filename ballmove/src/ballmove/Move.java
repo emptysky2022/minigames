0e2ea6074782fb94eed8e851bec06f5ae4c6c8ff
emptysky2022 <emptysky2022@gmail.com>
2022-11-08 20:17:08 +0900

    make movement more softly

--- ballmove/src/ballmove/Move.java
+++ ballmove/src/ballmove/Move.java
@@ -29,44 +29,36 @@ public class Move extends KeyAdapter implements Runnable{
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) 
 			PLAYER_LEFT = true;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) 
 			PLAYER_RIGHT = true;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
+		if(e.getKeyCode() == KeyEvent.VK_UP) 
 			PLAYER_UP = true;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) 
 			PLAYER_DOWN = true;
-		}
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) 
 			PLAYER_LEFT = false;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) 
 			PLAYER_RIGHT = false;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
+		if(e.getKeyCode() == KeyEvent.VK_UP) 
 			PLAYER_UP = false;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) 
 			PLAYER_DOWN = false;
-		}
 	}
 	@Override
 	public void run() {
 		while(true) {
 			if(PLAYER_LEFT) 
 				r.x -= 1;
-			else if(PLAYER_RIGHT)
+			if(PLAYER_RIGHT)
 				r.x += 1;
-			else if(PLAYER_UP)
+			if(PLAYER_UP)
 				r.y -= 1;
-			else if (PLAYER_DOWN) 
+			if (PLAYER_DOWN) 
 				r.y += 1;
 			player.setBounds(r);
 			try {
